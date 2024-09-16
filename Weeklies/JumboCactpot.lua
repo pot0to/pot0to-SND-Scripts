b3e357a568b490421d3a621ff3e4e4691e41eccc
pot0to <19513249+pot0to@users.noreply.github.com>
2024-09-15 23:37:06 -0700

    turning jumbo cactpot into a state system

--- Weeklies/JumboCactpot.lua
+++ Weeklies/JumboCactpot.lua
@@ -1,64 +1,79 @@
-if not IsInZone(144) then
-    yield("/tp Gold Saucer")
-    yield("/wait 3")
-end
-while not IsInZone(144) do
-    yield("/wait 1")
-end
-PathfindAndMoveTo(-4.821474, 1.0425026, 2.2151544)
-while PathIsRunning() or PathfindInProgress() do
-    yield("/wait 1")
-end
-yield("/li Cactpot Board")
-yield("/wait 10")
-PathfindAndMoveTo(120.26248, 13.00115, -10.966645)
-while PathIsRunning() or PathfindInProgress() do
-    yield("/wait 1")
-end
-
--- target Mini Cactpot NPC
-repeat
-    yield("/target Jumbo Cactpot Broker")
-    yield("/wait 0.1")
-until HasTarget() and GetTargetName() == "Jumbo Cactpot Broker"
-repeat
-    yield("/interact")
-    yield("/wait 0.1")
-until GetCharacterCondition(32)
-
--- fast forward through dialog
-repeat
-    yield("/wait 0.1")
-until IsAddonVisible("Talk")
-yield("/wait 0.1")
-repeat
-    yield("/click Talk Click")
-    yield("/wait 0.1")
-until not IsAddonVisible("Talk")
-
--- purchase first ticket
-repeat
-    yield("/wait 0.1")
-until IsAddonVisible("SelectString")
-yield("/wait 0.1")
-yield("/callback SelectString true 0")
-yield("/wait 1")
-
--- for each cactpot ticket
-for i=0, 2 do
-    if IsAddonVisible("SelectYesno") then
-        yield("/callback SelectYesno true 0")
+function Start()
+    if not IsInZone(144) then
+        Teleport("Gold Saucer")
+        return
     end
-    repeat
-        yield("/wait 0.1")
-    until IsAddonVisible("LotteryWeeklyInput")
-    yield("/wait 1")
-    yield("/callback LotteryWeeklyInput true "..math.random(9999))
-    yield("/wait 0.1")
-    yield("/callback SelectYesno true 0")
-    yield("/wait 1")
+
+    yield("/target Aetheryte")
+
+    if not HasTarget() or GetTargetName() ~= "aetheryte" then
+        yield("/echo "..GetTargetName())
+        PathfindAndMoveTo(-4.82, 1.04, 2.21)
+    elseif GetDistanceToTarget() < 7 then
+        yield("/vnav stop")
+        yield("/li Cactpot Board")
+        State = CharacterStates.claimPrize
+    end
 end
-repeat
-    yield("/click Talk Click")
-    yield("/wait 0.1")
-until not IsAddonVisible("Talk")
+
+RewardClaimed = false
+function ClaimPrize()
+    yield("/target Cactpot Cashier")
+
+    if IsAddonVisible("LotteryWeeklyRewardList") then
+        yield("/callback LotteryWeeklyRewardList true -1")
+    elseif IsAddonVisible("SelectYesno") then
+        yield("/callback SelectYesno true 0")
+    elseif RewardClaimed and not GetCharacterCondition(32) then
+        State = CharacterStates.purchaseNewTickets
+    elseif not HasTarget() or GetTargetName() ~= "Cactpot Cashier" then
+        PathfindAndMoveTo(123.25, 13.00, -19.35)
+    elseif GetDistanceToTarget() < 7 then
+        yield("/vnav stop")
+        yield("/interact")
+        RewardClaimed = true
+    end
+end
+
+TicketsPurchased = false
+function PurchaseNewTickets()
+    yield("/target Jumbo Cactpot Broker")
+
+    if IsAddonVisible("LotteryWeeklyInput") then
+        yield("/wait 1")
+        yield("/callback LotteryWeeklyInput true "..math.random(9999))
+    elseif IsAddonVisible("SelectYesno") then
+        yield("/callback SelectYesno true 0")
+    elseif TicketsPurchased and not GetCharacterCondition(32) then
+        State = CharacterStates.endState
+    elseif not HasTarget() or GetTargetName() ~= "Jumbo Cactpot Broker" then
+        PathfindAndMoveTo(120.26, 13.00, -10.9)
+    elseif GetDistanceToTarget() < 7 then
+        yield("/vnav stop")
+        yield("/interact")
+        TicketsPurchased = true
+    end
+
+end
+
+function EndState()
+end
+
+CharacterStates =
+{
+    start = Start,
+    claimPrize = ClaimPrize,
+    purchaseNewTickets = PurchaseNewTickets,
+    endState = EndState
+}
+
+State = CharacterStates.start
+yield("/at y")
+while true do
+    if State == CharacterStates.endState then
+        break
+    else
+        State()
+        yield("/wait 0.1")
+    end
+end
