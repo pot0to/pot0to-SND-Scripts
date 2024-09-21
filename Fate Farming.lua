d24fe4459c2658c55db3a355ddef79b43b58b275
pot0to <19513249+pot0to@users.noreply.github.com>
2024-09-20 18:38:23 -0700

    updating version

--- Fate Farming.lua
+++ Fate Farming.lua
@@ -8,9 +8,10 @@ Created by: Prawellp, sugarplum done updates v0.1.8 to v0.1.9, pot0to
 
 ***********
 * Version *
-*  2.6.2  *
+*  2.7.0  *
 ***********
-    -> 2.6.2    Fixed npcs for Panaq Attack and Fire Suppression
+    -> 2.7.0    Rewrote parts of enemy pathing to avoid BMR follow
+                Fixed npcs for Panaq Attack and Fire Suppression
                 Fixing Pandora autotargeting, turned off bmrai follows, fixed pandora chocobo
                 Fixed RSR spam when BM/R is turned off, Cleaned up pandora checks
                 Added check for do fates getting pushed out of bounds
