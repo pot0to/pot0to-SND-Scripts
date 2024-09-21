0212addd352a8e3ae5daff1fe8e5dcd3552166d5
pot0to <19513249+pot0to@users.noreply.github.com>
2024-09-20 17:31:46 -0700

    Fixed npcs for Panaq Attack and Fire Suppression

--- Fate Farming.lua
+++ Fate Farming.lua
@@ -12,8 +12,7 @@ Created by: Prawellp, sugarplum done updates v0.1.8 to v0.1.9, pot0to
 ***********
     -> 2.6.2    Fixed npcs for Panaq Attack and Fire Suppression
                 Fixing Pandora autotargeting, turned off bmrai follows, fixed pandora chocobo
-                Fixed RSR spam when BM/R is turned off
-                Cleaned up pandora checks
+                Fixed RSR spam when BM/R is turned off, Cleaned up pandora checks
                 Added check for do fates getting pushed out of bounds
                 Fixing mount and leave after collections fate
                 Fixed collections fates table for Living Memory
