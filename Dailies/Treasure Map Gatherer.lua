f1b1a848c80ba7297c9c1e8fb68268bc40c45640
pot0to <19513249+pot0to@users.noreply.github.com>
2024-10-11 14:41:16 -0700

    Added ability to search entire timers list for map allowances, added

--- Dailies/Treasure Map Gatherer.lua
+++ Dailies/Treasure Map Gatherer.lua
@@ -7,11 +7,14 @@
 Created by: pot0to (https://ko-fi.com/pot0to)
 State Machine Diagram: https://github.com/pot0to/pot0to-SND-Scripts/blob/main/FateFarmingStateMachine.drawio.png
 
+Gathers a map, relogs as the next character in the list, and repeat.
+
 ********************************************************************************
 *                                    Version                                   *
-*                                     0.0.7                                    *
+*                                     0.0.8                                    *
 ********************************************************************************
-Gathers a map, relogs as the next character in the list, and repeat.
+0.0.8   Added ability to search entire timers list for map allowances, added
+            other maps
 
 ********************************************************************************
 *                               Required Plugins                               *
@@ -29,7 +32,7 @@ Gathers a map, relogs as the next character in the list, and repeat.
 ********************************************************************************
 ]]
 
-MapName = "Iron Ore"
+MapName = "Timeworn Gazelleskin Map"
 
 Multimode = true
 Characters =
@@ -47,10 +50,34 @@ Characters =
 ]]
 
 TimewornMapIds = {
+    -- ARR Maps
+    {itemId=6688, itemName="Timeworn Leather Map"},
+    {itemId=6689, itemName="Timeworn Goatskin Map"},
+    {itemId=6690, itemName="Timeworn Toadskin Map"},
+    {itemId=6691, itemName="Timeworn Boarskin Map"},
+    {itemId=6692, itemName="Timeworn Piesteskin Map"},
+
+    -- Heavensward Maps
+    {itemId=12241, itemName="Timeworn Archaeoskin Map"},
+    {itemId=12242, itemName="Timeworn Wyvernskin Map"},
+    {itemId=12243, itemName="Timeworn Dragonskin Map"},
+    
+    -- Stormblood Maps
+    {itemId=17835, itemName="Timeworn Gaganaskin Map"},
     {itemId=17836, itemName="Timeworn Gazelleskin Map"},
-    {itemId=43557, itemName="Timeworn Br'aaxskin Map"},
+
+    -- Shadowbringers Maps
+    {itemId=26744, itemName="Timeworn Gliderskin Map"},
+    {itemId=26745, itemName="Timeworn Zonureskin Map"},
+    
+    -- Endwalker Maps
+    {itemId=36611, itemName="Timeworn Saigaskin Map"},
+    {itemId=36612, itemName="Timeworn Kumbhiraskin Map"},
+    {itemId=39591, itemName="Timeworn Ophiotauroskin Map"},
+
+    -- Dawntrail Maps
     {itemId=43556, itemName="Timeworn Loboskin Map"},
-    {itemId=5111, itemName="Iron Ore"}
+    {itemId=43557, itemName="Timeworn Br'aaxskin Map"}
 }
 
 function GetMapInfo()
@@ -59,6 +86,21 @@ function GetMapInfo()
             return mapInfo
         end
     end
+end
+
+function HasMapAllowance()
+    if not IsAddonVisible("ContentsInfo") then
+        yield("/timers")
+        yield ("/wait 1")
+    end
+
+    for i = 1, 15 do
+        local timerName = GetNodeText("ContentsInfo", 8, i, 5)
+        if timerName == "Next Map Allowance" then
+            return GetNodeText("ContentsInfo", 8, i, 4) == "Available Now"
+        end
+    end
+    return false
 end
 
 function Gather()
@@ -106,7 +148,7 @@ function Main()
 
     yield("/echo "..MapInfo.itemId)
     yield("/echo "..GetItemCount(MapInfo.itemId))
-    if GetItemCount(MapInfo.itemId) > 0 or hasMapAllowance ~= "Available Now" then
+    if GetItemCount(MapInfo.itemId) > 0 or not HasMapAllowance() then
         if GBRAutoOn then
             yield("/gbr auto off")
             GBRAutoOn = false
@@ -115,7 +157,12 @@ function Main()
             return
         end
 
-        yield("/echo No map allowance left for today.")
+        if GetItemCount(MapInfo.itemId) > 0 then
+            yield("/echo Already have map in inventory.")
+        end
+        if not HasMapAllowance() then
+            yield("/echo No map allowance left for today.")
+        end
 
         yield("/li auto")
         yield("/wait 1")
